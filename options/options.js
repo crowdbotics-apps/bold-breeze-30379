09b664b97802a7ec8821068cc88285a308bd09aa
Crowdbotics <team@crowdbotics.com>
2021-09-08 00:53:51 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "bold_breeze_30379Identifier",
+  url: "https://bold_breeze_30379Identifier.botics.co",
+  api: "https://bold_breeze_30379Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
